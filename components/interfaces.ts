ab78f39e76db5465075a0427bfa5938ddab8ca57
Cleudeir <Cleudeirsilva@gmail.com>
2023-01-05 01:43:16 -0300

    update

--- components/interfaces.ts
+++ components/interfaces.ts
@@ -28,6 +28,18 @@ export interface DataTv {
   url: string;
   title: string;
   episodes?: episode[];
+  adult?: boolean;
+  backdrop_path?: string;
+  genre_ids?: number[];
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: string;
+  poster_path?: string;
+  release_date?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
 }
 export interface Data {
   movie: DataMovie[];
