9d0638e526fe66ada4ea2c956f925d4629c18756
Cleudeir <Cleudeirsilva@gmail.com>
2023-01-10 23:07:18 -0300

    update

--- pages/series/[id].tsx
+++ pages/series/[id].tsx
@@ -22,7 +22,6 @@ function urlTransform(router: any): DataTv {
     title,
     episodes: [],
   }
-  console.log(obj)
   return obj
 }
 
@@ -35,6 +34,13 @@ export async function getStaticProps(context: { params: { id: string } }) {
     headers: { 'Content-type': 'application/json; charset=UTF-8' },
   })
   const data = await get.json()
+  if (!data) {
+    fetch(`${process.env.BACK_URL}/api/deleteTv`, {
+      method: 'DELETE',
+      body: JSON.stringify({ item }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    })
+  }
   return {
     props: { data },
     revalidate: 30 * 24 * 60 * 60,
