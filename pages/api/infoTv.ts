5886781e7cce1d55bd55fdfeb2304c5e97bac838
Cleudeir <Cleudeirsilva@gmail.com>
2023-01-10 22:51:27 -0300

    update

---
+++ pages/api/infoTv.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { episode } from '../../components/interfaces'
+
+
+export default async function infoTv(
+  req: NextApiRequest,
+  res: NextApiResponse<episode | null>
+): Promise<void> {
+  const { item } = req.body
+  try {
+    const data = await fetch(`${process.env.BACK_URL}/api/infoTv`, {
+      method: 'POST',
+      body: JSON.stringify({ item }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    })
+    const json = await data.json()
+    res.status(200).json(json)
+  } catch (error) {
+    console.warn(error)
+    res.status(500).json(null)
+  }
+}
--- pages/series/[id].tsx
+++ pages/series/[id].tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import styles from '../../styles/Pages.module.css'
-import { DataTv } from '../../components/interfaces'
+import { DataTv, episode } from '../../components/interfaces'
 import Link from 'next/link'
+import Loading from '../../components/Loading'
 
 export async function getStaticPaths() {
   return {
@@ -24,58 +25,85 @@ function urlTransform(router: any): DataTv {
   console.log(obj)
   return obj
 }
+
 export async function getStaticProps(context: { params: { id: string } }) {
   const { id } = context.params
   const item = urlTransform(id)
-  const data = await fetch(`${process.env.BACK_URL}/api/infoTv`, {
+  const get = await fetch(`${process.env.BACK_URL}/api/infoTvList`, {
     method: 'POST',
     body: JSON.stringify({ item }),
     headers: { 'Content-type': 'application/json; charset=UTF-8' },
   })
-  const video = await data.json()
+  const data = await get.json()
   return {
-    props: { video },
+    props: { data },
     revalidate: 30 * 24 * 60 * 60,
   }
 }
+async function getInfo({ item }: { item: episode }): Promise<DataTv | null> {
+  try {
+    const data = await fetch(`/api/infoTv`, {
+      method: 'POST',
+      body: JSON.stringify({ item }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    })
+    const json = await data.json()
+    return json
+  } catch (error) {
+    console.warn(error)
+    return null
+  }
+}
 
-export default function movieId({ video }: { video: DataTv }): JSX.Element {
+export default function movieId({ data }: { data: DataTv }): JSX.Element {
   const [index, setIndex] = useState<number>(0)
+  const [video, setVideo] = useState<episode | null>(null)
   const [counter, setCounter] = useState(3)
+
+  useEffect(() => {}, [])
 
   useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000)
-  }, [counter])
+  }, [])
 
   useEffect(() => {
-    if (!video) {
+    if (!data) {
       setTimeout(() => {
         window.location.href = '/series'
       }, 3000)
     }
 
-    if (video?.title) {
-      const storage = Number(localStorage.getItem(video.title))
+    if (data?.title) {
+      const storage = localStorage.getItem(data.title)
       if (storage) {
-        setIndex(storage)
+        setVideo(JSON.parse(storage))
+      } else {
+        void (async () => {
+          const item = data.episodes[0]
+          const _video = await getInfo({ item })
+          setVideo(_video)
+        })()
       }
     }
   }, [])
-  function changeIndex(e: number): void {
-    if (video) {
-      let _index = index + e
-      const episodesLength = video.episodes.length || 0
+  async function changeIndex(e: number): Promise<void> {
+    if (data && video) {
+      setVideo(null)
+      let _index = video.id + e
+      const episodesLength = data.episodes.length
       if (_index > episodesLength - 1) {
         _index = episodesLength - 1
       }
       if (_index < 0) {
         _index = 0
       }
-      localStorage.setItem(video.title, String(_index))
-      setIndex(_index)
+      const item = data.episodes[_index]
+      const _video = await getInfo({ item })
+      localStorage.setItem(data.title, JSON.stringify(_video))
+      setVideo(_video)
     }
   }
-  if (!video) {
+  if (!data) {
     return (
       <div className={styles.iframe}>
         <div className={styles.error}>
@@ -85,41 +113,46 @@ export default function movieId({ video }: { video: DataTv }): JSX.Element {
       </div>
     )
   }
+  if (!video) {
+    return <Loading />
+  }
   return (
-    <div className={styles.iframe}>
-      <div className={styles.buttons}>
-        <Link href={'/series'}>
-          <button type="button">Home</button>
-        </Link>
-        <div className={styles.legend}>
-          <h2>{video.title}</h2>
+    video && (
+      <div className={styles.iframe}>
+        <div className={styles.buttons}>
+          <Link href={'/series'}>
+            <button type="button">Home</button>
+          </Link>
+          <div className={styles.legend}>
+            <h2>{data.title}</h2>
+          </div>
+          <h2>
+            {video.id + 1}/{data.episodes.length}
+          </h2>
+          <button
+            type="button"
+            onClick={() => {
+              changeIndex(-1)
+            }}
+          >
+            Return
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              changeIndex(1)
+            }}
+          >
+            Next
+          </button>
         </div>
-        <h2>
-          {video.episodes[index].id + 1}/{video.episodes.length}
-        </h2>
-        <button
-          type="button"
-          onClick={() => {
-            changeIndex(-1)
-          }}
-        >
-          Return
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            changeIndex(1)
-          }}
-        >
-          Next
-        </button>
+        <iframe
+          name="Player"
+          frameBorder={0}
+          src={'https://sinalpublico.com' + video.url}
+          allowFullScreen
+        ></iframe>
       </div>
-      <iframe
-        name="Player"
-        frameBorder={0}
-        src={'https://sinalpublico.com' + video.episodes[index].url}
-        allowFullScreen
-      ></iframe>
-    </div>
+    )
   )
 }
